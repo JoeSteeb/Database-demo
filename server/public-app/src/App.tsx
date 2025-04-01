91e1af7ee9c939b666320774a84d368a69d9d67d
JoeSteeb <josteeb@gmail.com>
2025-03-31 22:30:41 -0700

    Assembled basic react frontend.

--- server/public-app/src/App.tsx
+++ server/public-app/src/App.tsx
@@ -1,12 +1,32 @@
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import { People } from "./pages/people";
+import { Tips } from "./pages/tips";
 
 function App() {
+  const pages = [
+    { title: "People", content: <People /> },
+    { title: "Tips", content: <Tips /> },
+  ];
+
+  const [currentPage, setCurrentPage] = useState(<People />);
 
   return (
-    <>
-      <h1>Public App</h1>
-    </>
-  )
+    <div className="flex flex-col w-screen h-screen">
+      <div className="w-screen flex align-middle justify-center bg-amber-50">
+        {pages.map((p) => (
+          <button
+            onClick={() => setCurrentPage(p.content)}
+            key={p.title}
+            className="mx-2"
+          >
+            {p.title}
+          </button>
+        ))}
+      </div>
+      <div className="flex-grow w-screen bg-amber-200">{currentPage}</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ server/public-app/src/pages/tips.tsx
@@ -0,0 +1,3 @@
+export const Tips = () => {
+  return <div>Tips</div>;
+};
